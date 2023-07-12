4b2352d2347b489de834a80306b49cc751966c25
Kevin Jessie <kevinjessie32@yahoo.com>
2023-07-11 17:46:28 -0700

    schemas done

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,71 @@
+// set up path to resolvers
+
+// Path: server/schemas/resolvers.js
+// import the user-defined GraphQL type
+const { User, Book } = require('../models');
+// import the signToken() function from the utils folder
+const { signToken } = require('../utils/auth');
+
+// Path: server/schemas/resolvers.js
+// add the following to the end of the file
+const resolvers = {
+    Query: { 
+        // get a single user by either their id or their username
+        me: async (parent, args, context) => {
+            if (context.user) {
+                const userData = await User.findOne({ _id: context.user._id })
+                    .select('-__v -password')
+                    .populate('books')
+                return userData;
+            }
+            throw new AuthenticationError('Not logged in');
+        }
+    },
+    Mutation: {
+        // create a user, sign a token, and send it back (to client/src/components/SignUpForm.js)
+        addUser: async (parent, args) => {
+            const user = await User.create(args);
+            const token = signToken(user);
+            return { token, user };
+        },
+        // login a user, sign a token, and send it back (to client/src/components/LoginForm.js)
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+            if (!user) {
+                throw new AuthenticationError('Incorrect credentials');
+            }
+            const correctPw = await user.isCorrectPassword(password);
+            if (!correctPw) {
+                throw new AuthenticationError('Incorrect credentials');
+            }
+            const token = signToken(user);
+            return { token, user };
+        },
+        // save a book to a user's `savedBooks` field by adding it to the set (to prevent duplicates)
+        // user comes from `req.user` created in the auth middleware function
+        saveBook: async (parent, { bookData }, context) => { 
+            if (context.user) {
+                const updatedUser = await User.findByIdAndUpdate(
+                    { _id: context.user._id },
+                    { $addToSet: { savedBooks: bookData } },
+                    { new: true }
+                );
+                return updatedUser;
+            }
+            throw new AuthenticationError('You need to be logged in!');
+        },
+        // remove a book from `savedBooks`
+        removeBook: async (parent, { bookId }, context) => {
+            if (!context.user) {
+                throw new AuthenticationError('You need to be logged in!');
+            }
+        },
+    },
+};
+
+module.exports = resolvers;
+
+
+
+
+
--- server/utils/auth.js
+++ server/utils/auth.js
@@ -4,34 +4,36 @@ const jwt = require('jsonwebtoken');
 const secret = 'mysecretsshhhhh';
 const expiration = '2h';
 
+// set up middleware function for GraphQL authentication
 module.exports = {
-  // function for our authenticated routes
-  authMiddleware: function (req, res, next) {
-    // allows token to be sent via  req.query or headers
-    let token = req.query.token || req.headers.authorization;
-
+  authMiddleware: function ({ req }) {
+    // allows token to be sent via req.body, req.query, or headers
+    let token = req.body.token || req.query.token || req.headers.authorization;
+    console.log(token);
     // ["Bearer", "<tokenvalue>"]
     if (req.headers.authorization) {
       token = token.split(' ').pop().trim();
     }
-
+    console.log(token);
     if (!token) {
-      return res.status(400).json({ message: 'You have no token!' });
+      return req;
     }
 
-    // verify token and get user data out of it
     try {
+      // decode and attach user data to request object
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
+      console.log(data);
       req.user = data;
+      console.log(req.user);
     } catch {
       console.log('Invalid token');
-      return res.status(400).json({ message: 'invalid token!' });
     }
 
-    // send to next endpoint
-    next();
+    // return updated request object
+    return req;
   },
   signToken: function ({ username, email, _id }) {
+    console.log(username, email, _id);
     const payload = { username, email, _id };
 
     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
